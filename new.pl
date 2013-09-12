1882d336471422cb6695b9f29291cf588f3a3ba1
Fwiffo <borgified@gmail.com>
2013-09-12 15:34:49 -0700

    testing log access

--- new.pl
+++ new.pl
@@ -13,15 +13,27 @@ my $gh = Net::GitHub::V3->new(
 
 my $gist = $gh->gist;
 
+my($worker,$build)=@ARGV;
+
+my $filename = "/var/lib/jenkins/jobs/assimmon/configurations/axis-label/$worker/builds/$build/log";
+
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!"
+
+while (my $row = <$fh>) {
+	chomp $row;
+	print "$row\n";
+}
+__END__
+
 $gist->create({
-		"description" => "the description for this gist",
-		"public" => 'true',
-		"files"  =>  {
-			"file1.txt" => {
-				"content" => "String file contents"
-			}
-		}
-	} 
+"description" => "the description for this gist",
+"public" => 'true',
+"files"  =>  {
+"file1.txt" => {
+"content" => "String file contents"
+}
+}
+} 
 );
 
 
